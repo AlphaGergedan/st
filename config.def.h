084048d0598e042f36a66eed92efc72d19cc5f46
Atamert Rahma <atamert.rahma@tum.de>
2026-04-28 12:19:29 +0200

    apply scrolling support patch via shift+pgup/down

--- config.def.h
+++ config.def.h
@@ -5,7 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
+//static char *font = "Iosevka-ExtendedHeavy:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=24:antialias=true:autohint=true";
 
 /* spare fonts */
 static char *sparefonts[] = {
@@ -266,6 +268,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,            selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,       selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,     numlock,        {.i =  0} },
+  { ShiftMask,            XK_Page_Up,      kscrollup,      {.i = -1} },
+  { ShiftMask,            XK_Page_Down,    kscrolldown,    {.i = -1} },
   { MODKEY,               XK_bracketleft,  chgalpha,       {.f = -1} },
   { MODKEY,               XK_bracketright, chgalpha,       {.f = +1} },
   { MODKEY,               XK_F1,           setpalette,     {.i = 0 } },
